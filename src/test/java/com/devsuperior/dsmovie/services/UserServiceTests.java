d51ec11ca309422f75979c1243f763fb05317e78
kaiquesilvadev <j.kaique1570@gmail.com>
2024-02-21 18:35:46 -0300

    test authenticatedShouldReturnUserEntityWhenUserExists ok

--- src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
@@ -1,10 +1,24 @@
 package com.devsuperior.dsmovie.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.devsuperior.dsmovie.entities.UserEntity;
+import com.devsuperior.dsmovie.repositories.UserRepository;
+import com.devsuperior.dsmovie.tests.UserFactory;
+import com.devsuperior.dsmovie.utils.CustomUserUtil;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration
@@ -12,9 +26,44 @@ public class UserServiceTests {
 
 	@InjectMocks
 	private UserService service;
+	
+	@Mock
+	private UserRepository repository;
+	
+	@Mock
+	private CustomUserUtil userUtil;
+	
+	private String username , userInexistente;
+	
+	private UserEntity userEntity;
+	
+	@BeforeEach
+	void setup() throws Exception {
+		//var
+		username = "maria@gmail.com";
+		userInexistente = "pedro@gmail.com";
+		userEntity = UserFactory.createUserEntity();
+		
+		// userUtil
+		Mockito.when(userUtil.getLoggedUsername()).thenReturn(username);
+		
+		//repository
+		Mockito.when(repository.findByUsername(username)).thenReturn(Optional.of(userEntity));
+		Mockito.when(repository.findByUsername(userInexistente)).thenReturn(Optional.empty());
 
+	}
+
+	@DisplayName("autenticado deve retornar a entidade do usuário quando o usuário existe")
 	@Test
 	public void authenticatedShouldReturnUserEntityWhenUserExists() {
+		
+		 UserEntity resultado = service.authenticated();
+		 
+		 assertNotNull(resultado);
+		 assertEquals(resultado, userEntity);
+		 assertEquals(resultado.getUsername(), username);
+		 assertNotNull(resultado.getPassword());
+		 assertNotNull(resultado.getRoles());
 	}
 
 	@Test
